e059b31b9bd5b545dbf6260e1dacb3ca1b8985ec
francescoLemmo <francesco.lemmo7@gmail.com>
2026-05-30 15:42:43 +0200

    Refactor Project and Packages. New Develop Branch created

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/* doesn't work with source level 1.8:
-module com.example.sad_project_7 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.example.sad_project_7 to javafx.fxml;
-    exports com.example.sad_project_7;
-}
-*/
