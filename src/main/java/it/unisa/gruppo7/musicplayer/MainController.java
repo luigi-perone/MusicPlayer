98247021309a6dfd235be6c0ae57f193095559c2
luigi-perone <luigi.perone1377@gmail.com>
2026-06-02 19:56:59 +0200

    css added to Sidebar

--- src/main/java/it/unisa/gruppo7/musicplayer/MainController.java
+++ src/main/java/it/unisa/gruppo7/musicplayer/MainController.java
@@ -2,18 +2,22 @@ package it.unisa.gruppo7.musicplayer;
 
 import it.unisa.gruppo7.musicplayer.library.LibraryController;
 import it.unisa.gruppo7.musicplayer.playlist.PlaylistSidebarController;
+import it.unisa.gruppo7.musicplayer.playlist.PlaylistService; // Importa il servizio
 import javafx.fxml.FXML;
 
 public class MainController {
 
-
     @FXML private PlaylistSidebarController playlistSidebarController;
-    @FXML private LibraryController libraryTableController;
+    @FXML private LibraryController libraryController; // Corretto il nome in base a fx:id="library"
 
     @FXML
     public void initialize() {
+        // 1. Inizializza il servizio (o passalo tramite costruttore/dipendenze se lo crei altrove)
+        PlaylistService playlistService = new PlaylistService();
 
-
-
+        // 2. Passa il servizio alla sidebar
+        if (playlistSidebarController != null) {
+            playlistSidebarController.setPlaylistService(playlistService);
+        }
     }
 }
