6df9536a7b58e4cde1b75028c7cc0f11c4ecd3b4
luigi-perone <luigi.perone1377@gmail.com>
2026-06-03 19:43:23 +0200

    test fixed

--- src/test/java/it/unisa/gruppo7/musicplayer/integration/TrackDeletionIntegrationTest.java
+++ src/test/java/it/unisa/gruppo7/musicplayer/integration/TrackDeletionIntegrationTest.java
@@ -1,54 +1,53 @@
 package it.unisa.gruppo7.musicplayer.integration;
 
-import it.unisa.gruppo7.musicplayer.library.Library;
+import it.unisa.gruppo7.musicplayer.musicplayerfacade.MusicPlayerFacade;
 import it.unisa.gruppo7.musicplayer.playlist.Playlist;
-import it.unisa.gruppo7.musicplayer.playlist.PlaylistService;
 import it.unisa.gruppo7.musicplayer.track.Track;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Year;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TrackDeletionIntegrationTest {
 
-    private Library library;
-    private PlaylistService playlistService;
+    private MusicPlayerFacade facade;
     private Track track;
-    private final String TEST_PLAYLIST_PATH = "data/test-playlist-delete.json";
 
     @BeforeEach
     public void setUp() {
-        library = Library.getInstance();
-        library.clearLibrary();
+        facade = MusicPlayerFacade.getInstance();
 
-        playlistService = new PlaylistService(TEST_PLAYLIST_PATH);
-        playlistService.getPlaylists().clear();
+        facade.getPlaylistService().getPlaylists().clear();
+        facade.getTracksFromLibrary().clear();
 
-        track = new Track("Song To Delete", "Test Author", 200, "Pop", Year.of(2021));
-        library.addTrack(track);
+        facade.addNewTrackToLibrary("Song To Delete", "Test Author", 200, "Pop", Year.of(2021));
 
+        track = facade.getTracksFromLibrary().stream()
+                .filter(t -> t.getTitle().equals("Song To Delete"))
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
+
+        facade.createPlaylist("My Playlist");
         List<Track> list = new ArrayList<>();
         list.add(track);
-
-        playlistService.createPlaylist("My Playlist");
-        playlistService.addTracksToPlaylist("My Playlist", list);
+        facade.getPlaylistService().addTracksToPlaylist("My Playlist", list);
     }
 
     @Test
     public void testTrackDeletionCascadesToPlaylists() {
-        Playlist playlist = playlistService.getPlaylist("My Playlist");
+        Playlist playlist = facade.getPlaylist("My Playlist");
 
-        assertNotNull(library.getTrackById(track.getId()));
+        assertNotNull(facade.getTrackFromLibrary(track.getId()));
         assertTrue(playlist.getPlaylist().contains(track));
 
-        library.removeTrack(track);
+        facade.removeTrackFromLibrary(track);
 
-        assertNull(library.getTrackById(track.getId()), "La traccia deve essere rimossa dalla libreria");
+        assertNull(facade.getTrackFromLibrary(track.getId()), "La traccia deve essere rimossa dalla libreria");
         assertFalse(playlist.getPlaylist().contains(track), "La traccia deve essere rimossa dalla playlist");
     }
 }
